436431c5d97412e902d89611476b577ef705346a
Lum1xi <mrlumixi@gmail.com>
2025-01-14 17:59:08 +0200

    lol

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from contact import *
 
 def main():
     while True:
-        print("1. Додавання нових контактів\n2. Знайти контакт\n3. Список контактів")
+        print("1. Додавання нових контактів: \n2. Знайти контакт: \n3. Список контактів")
         choise = input("Що робимо: ")
         if choise == "1":
-            print(add_contact(input("Введіть імя"), input("Введыть номер телефону"), input("Введіть email")))
+            print(add_contact(input("\nВведіть імя"), input("Введыть номер телефону"), input("Введіть email")))
         elif choise == "2":
-            print(search_contact(input("Введіть імя")))
+            print(search_contact(input("\nВведіть імя")))
 
         elif choise == "3":
             print(display_contacts())
